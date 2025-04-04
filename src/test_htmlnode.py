7acb026dbb7bbc918e203f60ef365cf6db30a53f
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-04-04 15:36:54 +0200

    Added LeafNode + related tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -52,6 +52,34 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.value, "Hello there")
         self.assertEqual(node.children, None)
         self.assertEqual(node.props, None)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, there!")
+        self.assertEqual(node.to_html(), "<p>Hello, there!</p>")
+
+    def test_leaf_to_html_a_href(self):
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node2.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_repr(self):
+        node = LeafNode(
+            "p",
+            "test paragraph node",
+            {
+                "href": "https://www.google.com",
+                "target": "_blank",
+            },
+        )
+        self.assertEqual(
+            repr(node),
+            "LeafNode(tag: p, text: test paragraph node, props: {'href': 'https://www.google.com', 'target': '_blank'})",
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello there!")
+        self.assertEqual(node.to_html(), "Hello there!")
 
 
 if __name__ == "__main__":
