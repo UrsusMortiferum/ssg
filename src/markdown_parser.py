b165f738b820625be5cbe94c2d7ba5e1128a4e17
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2026-04-04 16:53:36 +0200

    Added text_to_textnodes

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -2,6 +2,16 @@ import re
 from textnode import TextType, TextNode
 
 
+def text_to_textnodes(text):
+    node = TextNode(text, TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_link(new_nodes)
+    new_nodes = split_nodes_image(new_nodes)
+    return new_nodes
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     output = []
     for old_node in old_nodes:
@@ -81,3 +91,19 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!\!)\[(.*?)\]\((.*?)\)", text)
     return matches
+
+
+text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+# debug_helper("text conversion", text_to_textnodes(text))
+# [
+#     TextNode("This is ", TextType.TEXT),
+#     TextNode("text", TextType.BOLD),
+#     TextNode(" with an ", TextType.TEXT),
+#     TextNode("italic", TextType.ITALIC),
+#     TextNode(" word and a ", TextType.TEXT),
+#     TextNode("code block", TextType.CODE),
+#     TextNode(" and an ", TextType.TEXT),
+#     TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+#     TextNode(" and a ", TextType.TEXT),
+#     TextNode("link", TextType.LINK, "https://boot.dev"),
+# ]
--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -5,6 +5,7 @@ from markdown_parser import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 from textnode import TextNode, TextType
 
@@ -109,6 +110,27 @@ class TextMarkdownParser(unittest.TestCase):
                 TextNode(
                     "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
                 ),
+            ],
+        )
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            new_nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
             ],
         )
 
