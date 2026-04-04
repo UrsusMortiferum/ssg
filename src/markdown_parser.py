8e6901c61a2c2bbe5cf9e5cea446457a33e3a556
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2026-04-04 16:39:25 +0200

    Converted split_nodes_image to match style of split_nodes_link

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -29,27 +29,22 @@ def split_nodes_image(old_nodes):
         if old_node.text_type != TextType.TEXT:
             output.append(old_node)
             continue
-        matches = extract_markdown_images(old_node.text)
-        sections = []
         text = old_node.text
-        for i in range(len(matches)):
-            delimiter = f"![{matches[i][0]}]({matches[i][1]})"
-            tmp = text.split(delimiter, 1)
-            sections.append(tmp[0])
-            sections.append(matches[i])
-            text = tmp[1]
-        sections.append(text)
-        split_nodes = []
-        for i in range(len(sections)):
-            if sections[i] == "":
-                continue
-            if type(sections[i]) is str:
-                split_nodes.append(TextNode(sections[i], TextType.TEXT))
-            else:
-                split_nodes.append(
-                    TextNode(sections[i][0], TextType.IMAGE, sections[i][1])
-                )
-        output.extend(split_nodes)
+        matches = extract_markdown_images(old_node.text)
+        if len(matches) == 0:
+            output.append(old_node)
+            continue
+        for match in matches:
+            delimiter = f"![{match[0]}]({match[1]})"
+            sections = text.split(delimiter, 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown: image section not closed")
+            if sections[0] != "":
+                output.append(TextNode(sections[0], TextType.TEXT))
+            output.append(TextNode(match[0], TextType.IMAGE, match[1]))
+            text = sections[1]
+        if text != "":
+            output.append(TextNode(text, TextType.TEXT))
     return output
 
 
