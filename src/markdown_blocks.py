0d0a63b7adc10fdfb770c2981e4ac20102de2f5d
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2026-04-04 19:50:29 +0200

    Added a way to determine a type of block

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     out = []
@@ -7,3 +10,36 @@ def markdown_to_blocks(markdown):
             continue
         out.append(block)
     return out
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block):
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+
+    lines = block.split("\n")
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1] == "```":
+        return BlockType.CODE
+    quoted = list(filter(lambda line: line.startswith(">"), lines))
+    if len(quoted) == len(lines):
+        return BlockType.QUOTE
+    unordered = list(filter(lambda line: line.startswith("- "), lines))
+    if len(unordered) == len(lines):
+        return BlockType.UNORDERED_LIST
+    ordered = False
+    for i, line in enumerate(lines):
+        if line.startswith(f"{1 + i}. "):
+            ordered = True
+            continue
+        ordered = False
+    if ordered:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -22,6 +22,70 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type(self):
+        text = """
+Paragraph
+
+# Heading
+
+```
+Code
+```
+
+> Quote
+>Quote
+
+- Unordered list
+- Unordered list
+
+1. Ordered list
+2. Ordered list
+
+1. Paragraph
+3. Paragraph
+
+-Paragraph
+- Paragraph
+
+```paragraph```
+"""
+        blocks = markdown_to_blocks(text)
+        types = [block_to_block_type(b) for b in blocks]
+        self.assertListEqual(
+            types,
+            [
+                BlockType.PARAGRAPH,
+                BlockType.HEADING,
+                BlockType.CODE,
+                BlockType.QUOTE,
+                BlockType.UNORDERED_LIST,
+                BlockType.ORDERED_LIST,
+                BlockType.PARAGRAPH,
+                BlockType.PARAGRAPH,
+                BlockType.PARAGRAPH,
+            ],
+        )
+
+    def test_block_to_block_type_paragraph(self):
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_heading(self):
+        block = "## heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    # def test_block_to_block_type_not_code(self):
+    #     block = "```code\n```"
+    #     self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_not_code_alt(self):
+        block = "```\ncode```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
